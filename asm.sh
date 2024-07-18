8230278b12de02a64f3ea18025b3fddb15515ffb
debxrshi <dasdebarshi19@gmail.com>
2024-07-18 12:01:12 +0530

    ass

--- asm.sh
+++ asm.sh
@@ -14,3 +14,17 @@ echo -e '
 '
 
 # subdomain enum -> get origin ip -> filter cloudflare -> monitor for changes -> run nuclei
+
+
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo -e "\e[33m[-]\e[0m usage: submon.sh target.com HRS"
+    exit 1
+fi
+
+target=$1
+hrs=$2
+
+./submon.sh $target $hrs
+
+read active_subs.txt    
