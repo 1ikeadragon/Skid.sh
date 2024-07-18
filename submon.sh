bf23fc0661fbdd2422bda480141ffb1cce86bf96
debxrshi <dasdebarshi19@gmail.com>
2024-07-18 11:15:17 +0530

    optimized overall scripted, removed redundancy, increased accuracy

--- submon.sh
+++ submon.sh
@@ -36,59 +36,52 @@ function run_monitoring() {
     parse_provider_config
 
     echo -e "\e[32m[+]\e[0m Probing target with httpx\n"
-    host_probe=$(httpx -u "$target" -fr -sc -title -td -server -retries 3 -fc 404 -lc -t 500)
+    host_probe=$(httpx -u "$target" -fr -silent -sc -title -td -server -retries 3 -fc 404 -lc -t 500)
     if [ -z "$host_probe" ]; then
         echo -e "\n Host seems down. Did you type the right address?"
         exit 1
     fi
 
     echo -e "\n\e[32m[+]\e[0m Hunting subdomains with subfinder \n\n"
-    subs_subf=$(subfinder -d "$target" -all -recursive -silent -active)
+    subs=$(subfinder -d "$target" -all -recursive -silent)
 
-    # removing http https
-    all_subs=$(echo -e "$subs_subf" | sed 's|http[s]\?://||' | sort -u)
--
-    if [ -z "$all_subs" ]; then
-        echo -e "\n No subdomains found"
+    echo -e "\n\e[32m[+]\e[0m Probing subdomains with httpx\n\n"
+    active_subs=$(echo "$subs" | httpx -silent -random-agent -retries 3 -fc 404,301,302 -nc)
+
+    if [ -z "$active_subs" ]; then
+        echo -e "\n No active subdomains found"
     else
-        echo -e "\n\e[32m[+]\e[0m Probing subdomains with httpx\n\n"
-        active_subs=$(echo "$all_subs" | httpx -silent -random-agent -retries 3 -fc 404 -nc)
+        sorted_active_subs=$(echo "$active_subs" | sed 's|http[s]\?://||' | sort)
 
-        if [ -z "$active_subs" ]; then
-            echo -e "\n No active subdomains found"
-        else
-            sorted_active_subs=$(echo "$active_subs" | sed 's|http[s]\?://||' | sort)
+        if [ -f "active_subs.txt" ]; then
+            sorted_old_active_subs=$(sort active_subs.txt)
 
-            if [ -f "active_subs.txt" ]; then
-                sorted_old_active_subs=$(sort active_subs.txt | sed 's|http[s]\?://||')
+            changes=$(diff <(echo "$sorted_old_active_subs") <(echo "$sorted_active_subs"))
 
-                changes=$(diff <(echo "$sorted_old_active_subs") <(echo "$sorted_active_subs"))
+            if [ -n "$changes" ]; then
+                added_lines=$(echo "$changes" | grep "^>" | sed 's/^> //')
+                removed_lines=$(echo "$changes" | grep "^<" | sed 's/^< //')
 
-                if [ -n "$changes" ]; then
-                    added_lines=$(echo "$changes" | grep "^>" | sed 's/^> //')
-                    removed_lines=$(echo "$changes" | grep "^<" | sed 's/^< //')
-
-                    formatted_changes=""
-                    if [ -n "$added_lines" ]; then
-                        formatted_changes+="Added subdomains:\n\n$added_lines\n"
-                    fi
-                    if [ -n "$removed_lines" ]; then
-                        formatted_changes+="Removed subdomains:\n\n$removed_lines\n"
-                    fi
-
-                    echo -e "$formatted_changes" | notify -silent
-                else
-                    echo -e "\n\e[32m[+]\e[0m No changes detected in active subdomains.\n"
+                formatted_changes=""
+                if [ -n "$added_lines" ]; then
+                    formatted_changes+="Added subdomains:\n\n$added_lines\n"
                 fi
-            else
-                echo -e "\n\e[32m[+]\e[0m Creating active_subs.txt for the first time.\n"
-                echo "$sorted_active_subs" > active_subs.txt
-                echo "$sorted_active_subs" | notify -silent
-            fi
+                if [ -n "$removed_lines" ]; then
+                    formatted_changes+="Removed subdomains:\n\n$removed_lines\n"
+                fi
 
-            echo -e "\n\e[32m[+]\e[0m Active subdomains:\n"
-            echo "$sorted_active_subs"
+                echo -e "$formatted_changes" | notify -silent
+            else
+                echo -e "\n\e[32m[+]\e[0m No changes detected in active subdomains.\n"
+            fi
+        else
+            echo -e "\n\e[32m[+]\e[0m Creating active_subs.txt for the first time.\n"
+            echo "$sorted_active_subs" > active_subs.txt
+            echo "$sorted_active_subs" | notify -silent
         fi
+
+        echo -e "\n\e[32m[+]\e[0m Active subdomains:\n"
+        echo "$sorted_active_subs"
     fi
 }
 
